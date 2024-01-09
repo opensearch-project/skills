7a596a0c0c70cf70d3eeccf525991db322bbeaa7
Hailong Cui <ihailong@amazon.com>
2024-01-09 12:49:39 +0800

    fix compile error (#114)
    * fix compile error
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    * fix compile error
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    ---------
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- src/main/java/org/opensearch/agent/tools/SearchAnomalyDetectorsTool.java
+++ src/main/java/org/opensearch/agent/tools/SearchAnomalyDetectorsTool.java
@@ -281,6 +281,16 @@ public class SearchAnomalyDetectorsTool implements Tool {
         public String getDefaultDescription() {
             return DEFAULT_DESCRIPTION;
         }
+
+        @Override
+        public String getDefaultType() {
+            return TYPE;
+        }
+
+        @Override
+        public String getDefaultVersion() {
+            return null;
+        }
     }
 
 }
