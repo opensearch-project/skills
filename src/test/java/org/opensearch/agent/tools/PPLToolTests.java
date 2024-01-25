0ff76c19621e39c5933d88762b4c09eaf58e7117
xinyual <xinyual@amazon.com>
2024-01-25 12:00:18 +0800

    add execute field
    Signed-off-by: xinyual <xinyual@amazon.com>

--- src/test/java/org/opensearch/agent/tools/PPLToolTests.java
+++ src/test/java/org/opensearch/agent/tools/PPLToolTests.java
@@ -139,6 +139,17 @@ public class PPLToolTests {
     }
 
     @Test
+    public void testTool_with_WithoutExecution() {
+        PPLTool tool = PPLTool.Factory.getInstance().create(ImmutableMap.of("model_id", "modelId", "model_type", "claude", "execute", false));
+        assertEquals(PPLTool.TYPE, tool.getName());
+
+        tool.run(ImmutableMap.of("index", "demo", "question", "demo"), ActionListener.<String>wrap(executePPLResult -> {
+            assertEquals("source=demo| head 1", executePPLResult);
+        }, e -> { log.info(e); }));
+
+    }
+
+    @Test
     public void testTool_with_DefaultPrompt() {
         PPLTool tool = PPLTool.Factory.getInstance().create(ImmutableMap.of("model_id", "modelId", "model_type", "claude"));
         assertEquals(PPLTool.TYPE, tool.getName());
