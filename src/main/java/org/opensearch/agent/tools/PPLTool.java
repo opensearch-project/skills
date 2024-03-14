67268bd5494ff4fe05989893af5b3eae022c532d
xinyual <xinyual@amazon.com>
2024-03-14 12:52:29 +0800

    add parameter validate for PPL tool
    Signed-off-by: xinyual <xinyual@amazon.com>

--- src/main/java/org/opensearch/agent/tools/PPLTool.java
+++ src/main/java/org/opensearch/agent/tools/PPLTool.java
@@ -23,7 +23,9 @@ import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.text.StringSubstitutor;
 import org.json.JSONObject;
 import org.opensearch.action.ActionRequest;
@@ -313,6 +315,7 @@ public class PPLTool implements Tool {
 
         @Override
         public PPLTool create(Map<String, Object> map) {
+            validatePPLToolParameters(map);
             return new PPLTool(
                 client,
                 (String) map.get("model_id"),
@@ -354,6 +357,33 @@ public class PPLTool implements Tool {
         GetMappingsRequest getMappingsRequest = new GetMappingsRequest();
         getMappingsRequest.indices(indices);
         return getMappingsRequest;
+    }
+
+    private static void validatePPLToolParameters(Map<String, Object> map){
+        if (!map.containsKey("model_id")){
+            throw new IllegalArgumentException("PPL tool needs model id.");
+        } else {
+            String modelId = (String) map.get("model_id");
+            if (StringUtils.isBlank(modelId)){
+                throw new IllegalArgumentException("PPL tool needs non blank model id.");
+            }
+        }
+        if (map.containsKey("execute")) {
+            String  execute = map.get("execute").toString().toLowerCase(Locale.ROOT);
+            if (!(execute.equals("true")) && !(execute.equals("false"))) {
+                throw new IllegalArgumentException("PPL tool parameter execute must be false or true");
+            }
+
+        }
+        if (map.containsKey("head")){
+            String head = map.get("head").toString();
+            try {
+                int headInt = NumberUtils.createInteger(head);
+            }
+            catch (Exception e) {
+                throw new IllegalArgumentException("PPL tool parameter head must be integer.");
+            }
+        }
     }
 
     private String constructTableInfo(SearchHit[] searchHits, Map<String, MappingMetadata> mappings) throws PrivilegedActionException {
--- src/test/java/org/opensearch/agent/tools/PPLToolTests.java
+++ src/test/java/org/opensearch/agent/tools/PPLToolTests.java
@@ -126,6 +126,54 @@ public class PPLToolTests {
     }
 
     @Test
+    public void testTool_WithoutModelId() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PPLTool.Factory.getInstance().create(ImmutableMap.of("prompt", "contextPrompt"))
+        );
+        assertEquals(
+                "PPL tool needs model id.",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    public void testTool_WithBlankModelId() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PPLTool.Factory.getInstance().create(ImmutableMap.of("model_id", "   "))
+        );
+        assertEquals(
+                "PPL tool needs non blank model id.",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    public void testTool_WithNonIntegerHead() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PPLTool.Factory.getInstance().create(ImmutableMap.of("model_id", "demo", "head", "11.5"))
+        );
+        assertEquals(
+                "PPL tool parameter head must be integer.",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    public void testTool_WithNonBooleanExecute() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PPLTool.Factory.getInstance().create(ImmutableMap.of("model_id", "demo", "execute", "hello"))
+        );
+        assertEquals(
+                "PPL tool parameter execute must be false or true",
+                exception.getMessage()
+        );
+    }
+
+    @Test
     public void testTool() {
         PPLTool tool = PPLTool.Factory.getInstance().create(ImmutableMap.of("model_id", "modelId", "prompt", "contextPrompt"));
         assertEquals(PPLTool.TYPE, tool.getName());
