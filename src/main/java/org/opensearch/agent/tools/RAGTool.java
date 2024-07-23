71013397da24013aa18b18018cc34ebd16f27fc8
zane-neo <zaniu@amazon.com>
2024-07-23 10:19:10 +0800

    Support multi tenancy minimal changes
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- src/main/java/org/opensearch/agent/tools/RAGTool.java
+++ src/main/java/org/opensearch/agent/tools/RAGTool.java
@@ -144,7 +144,7 @@ public class RAGTool implements Tool {
 
             RemoteInferenceInputDataSet inputDataSet = RemoteInferenceInputDataSet.builder().parameters(tmpParameters).build();
             MLInput mlInput = MLInput.builder().algorithm(FunctionName.REMOTE).inputDataset(inputDataSet).build();
-            ActionRequest request = new MLPredictionTaskRequest(this.inferenceModelId, mlInput, null);
+            ActionRequest request = new MLPredictionTaskRequest(this.inferenceModelId, mlInput, null, null);
 
             client.execute(MLPredictionTaskAction.INSTANCE, request, ActionListener.wrap(resp -> {
                 ModelTensorOutput modelTensorOutput = (ModelTensorOutput) resp.getOutput();
