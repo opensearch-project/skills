3aa759d68d7f8f54c94698bfe153f5df3209a171
Heng Qian <qianheng@amazon.com>
2024-09-29 18:41:54 +0800

    spotlessApply
    Signed-off-by: Heng Qian <qianheng@amazon.com>

--- src/main/java/org/opensearch/agent/tools/LogPatternTool.java
+++ src/main/java/org/opensearch/agent/tools/LogPatternTool.java
@@ -15,8 +15,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
-
 import java.util.stream.Collectors;
+
 import org.apache.commons.lang3.StringUtils;
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.search.SearchResponse;
