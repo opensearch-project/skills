5144fdf1b9e3fae32c2cdc678ffd4f85dab18605
Hailong Cui <ihailong@amazon.com>
2025-01-24 09:11:40 +0800

    add multi tenacy support
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- src/main/java/org/opensearch/agent/tools/PPLTool.java
+++ src/main/java/org/opensearch/agent/tools/PPLTool.java
@@ -5,6 +5,8 @@
 
 package org.opensearch.agent.tools;
 
+import static org.opensearch.ml.common.CommonValue.TENANT_ID_FIELD;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -172,6 +174,7 @@ public class PPLTool implements Tool {
     @SuppressWarnings("unchecked")
     @Override
     public <T> void run(Map<String, String> parameters, ActionListener<T> listener) {
+        final String tenantId = parameters.get(TENANT_ID_FIELD);
         extractFromChatParameters(parameters);
         String indexName = getIndexNameFromParameters(parameters);
         if (StringUtils.isBlank(indexName)) {
@@ -209,7 +212,8 @@ public class PPLTool implements Tool {
                 ActionRequest request = new MLPredictionTaskRequest(
                     modelId,
                     MLInput.builder().algorithm(FunctionName.REMOTE).inputDataset(inputDataSet).build(),
-                    null
+                    null,
+                    tenantId
                 );
                 client.execute(MLPredictionTaskAction.INSTANCE, request, ActionListener.wrap(mlTaskResponse -> {
                     ModelTensorOutput modelTensorOutput = (ModelTensorOutput) mlTaskResponse.getOutput();
