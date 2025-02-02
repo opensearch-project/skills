fe67f7e72fb8903a7d2527df9630d27a072e734c
xinyual <xinyual@amazon.com>
2025-02-02 19:04:16 +0800

    try
    Signed-off-by: xinyual <xinyual@amazon.com>

--- src/test/java/org/opensearch/integTest/ToolIntegrationTest.java
+++ src/test/java/org/opensearch/integTest/ToolIntegrationTest.java
@@ -19,7 +19,6 @@ import org.opensearch.client.RequestOptions;
 import org.opensearch.client.Response;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.sun.net.httpserver.HttpServer;
 
@@ -45,13 +44,6 @@ public abstract class ToolIntegrationTest extends BaseAgentToolsIT {
         server.start();
         clusterSettings(false);
         connectorId = setUpConnectorWithRetry(5);
-        TimeUnit.SECONDS.sleep(3);
-        JsonObject tmp = queryConnector();
-        log.info(tmp);
-        log.debug(tmp);
-        log.error(tmp);
-        // assertTrue(Objects.equals(tmp.get("hits").getAsJsonObject().get("total").getAsJsonObject().get("value").getAsString(), "0"));
-        // assertTrue(tmp.get("hits").getAsJsonObject().get("total").getAsJsonObject().get("value").getAsInt() > 0);
         modelGroupId = setupModelGroup();
         modelId = setupLLMModel(connectorId, modelGroupId);
         // wait for model to get deployed
@@ -91,7 +83,6 @@ public abstract class ToolIntegrationTest extends BaseAgentToolsIT {
                 TimeUnit.SECONDS.sleep(20);
             }
         }
-        queryConnector();
         return connectorId;
     }
 
@@ -166,16 +157,6 @@ public abstract class ToolIntegrationTest extends BaseAgentToolsIT {
         return JsonParser.parseString(resp).getAsJsonObject().get("model_group_id").getAsString();
     }
 
-    private JsonObject queryConnector() throws IOException {
-        Request request = new Request("POST", "/.plugins-ml-connector/_search");
-        request.setJsonEntity("{\n" + "    \"query\": {\n" + "      \"match_all\": {}\n" + "} \n" + "} \n");
-        Response response = executeRequest(request);
-
-        String resp = readResponse(response);
-
-        return JsonParser.parseString(resp).getAsJsonObject();
-    }
-
     private String setupLLMModel(String connectorId, String modelGroupId) throws IOException {
         Request request = new Request("POST", "/_plugins/_ml/models/_register?deploy=true");
         request
@@ -245,7 +226,7 @@ public abstract class ToolIntegrationTest extends BaseAgentToolsIT {
         RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
         builder.addHeader("Content-Type", "application/json");
         request.setOptions(builder);
-        return adminClient().performRequest(request);
+        return client().performRequest(request);
     }
 
     public static String readResponse(Response response) throws IOException {
