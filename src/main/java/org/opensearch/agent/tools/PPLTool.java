b9c8869d490505a202165abecfb74951cfd98872
xinyual <xinyual@amazon.com>
2025-06-16 14:28:51 +0800

    merge MetaData
    Signed-off-by: xinyual <xinyual@amazon.com>

--- src/main/java/org/opensearch/agent/tools/PPLTool.java
+++ src/main/java/org/opensearch/agent/tools/PPLTool.java
@@ -36,6 +36,7 @@ import org.opensearch.action.ActionRequest;
 import org.opensearch.action.admin.indices.mapping.get.GetMappingsRequest;
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.agent.tools.utils.ToolHelper;
+import org.opensearch.agent.tools.utils.mergeMetaData.MergeRuleHelper;
 import org.opensearch.cluster.metadata.MappingMetadata;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.action.ActionResponse;
@@ -289,6 +290,7 @@ public class PPLTool implements WithModelTool {
 
             return;
         }
+
         GetMappingsRequest getMappingsRequest = buildGetMappingRequest(indexName);
         client.admin().indices().getMappings(getMappingsRequest, ActionListener.wrap(getMappingsResponse -> {
             Map<String, MappingMetadata> mappings = getMappingsResponse.getMappings();
@@ -505,16 +507,33 @@ public class PPLTool implements WithModelTool {
     }
 
     private String constructTableInfo(SearchHit[] searchHits, Map<String, MappingMetadata> mappings) throws PrivilegedActionException {
-        String firstIndexName = (String) mappings.keySet().toArray()[0];
-        MappingMetadata mappingMetadata = mappings.get(firstIndexName);
-        Map<String, Object> mappingSource = (Map<String, Object>) mappingMetadata.getSourceAsMap().get("properties");
-        if (Objects.isNull(mappingSource)) {
+        if (mappings.keySet().size() == 0) {
             throw new IllegalArgumentException(
                 "The querying index doesn't have mapping metadata, please add data to it or using another index."
             );
         }
+        Map<String, Object> allFields = new HashMap<>();
+        for  (Object index:mappings.keySet().toArray()) {
+            MappingMetadata mappingMetadata = mappings.get(index);
+            Map<String, Object> mappingSource = (Map<String, Object>) mappingMetadata.getSourceAsMap().get("properties");
+            MergeRuleHelper.merge(mappingSource, allFields);
+        }
+        log.info("all fields");
+        log.info(allFields.toString());
+        log.info("--------------------");
+        log.error("all fields");
+        log.error(allFields.toString());
+        log.error("--------------------");
         Map<String, String> fieldsToType = new HashMap<>();
-        ToolHelper.extractFieldNamesTypes(mappingSource, fieldsToType, "", false);
+        ToolHelper.extractFieldNamesTypes(allFields, fieldsToType, "", false);
+
+        log.info("all fields result");
+        log.info(fieldsToType.toString());
+        log.info("--------------------");
+        log.error("all fields result");
+        log.error(fieldsToType.toString());
+        log.error("--------------------");
+
         StringJoiner tableInfoJoiner = new StringJoiner("\n");
         List<String> sortedKeys = new ArrayList<>(fieldsToType.keySet());
         Collections.sort(sortedKeys);
---
+++ src/main/java/org/opensearch/agent/tools/utils/mergeMetaData/DeepMergeRule.java
@@ -0,0 +1,24 @@
+package org.opensearch.agent.tools.utils.mergeMetaData;
+
+import java.util.Map;
+
+/** This rule will merge two array/struct object and merge their properties */
+public class DeepMergeRule implements MergeRule {
+
+    @Override
+    public boolean isMatch(Map<String, Object> source, Map<String, Object> target) {
+        return source != null
+               && target != null
+               && source.get("properties") != null
+               && target.get("properties") != null && source.get("type") == target.get("type");
+    }
+
+    @Override
+    public void mergeInto(
+            String key, Map<String, Object> source, Map<String, Object> target) {
+        Map<String, Object> existing = (Map<String, Object>) target.get(key);
+        MergeRuleHelper.merge((Map<String, Object>) source.get("properties"), (Map<String, Object>) existing.get("properties"));
+        target.put(key, existing);
+    }
+}
+
