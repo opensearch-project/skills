9435199685250769f249b0e3021173124396351e
Yuanchun Shen <yuanchu@amazon.com>
2025-08-08 17:02:49 +0800

    Add parameter extraction utilities for tool inputs
    - Add utilities for extracting required parameters and JSON input parameters
    - Apply parameter extraction in AbstractRetrieverTool and RAGTool
    - Define TOOL_REQUIRED_PARAMS constant for consistent parameter handling
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- src/main/java/org/opensearch/agent/tools/utils/ToolHelper.java
+++ src/main/java/org/opensearch/agent/tools/utils/ToolHelper.java
@@ -5,16 +5,22 @@
 
 package org.opensearch.agent.tools.utils;
 
+import static org.opensearch.ml.common.utils.StringUtils.gson;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.text.StringSubstitutor;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.action.ActionResponse;
 import org.opensearch.ml.common.utils.StringUtils;
 import org.opensearch.sql.plugin.transport.TransportPPLQueryResponse;
+
+import com.google.gson.reflect.TypeToken;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -88,5 +94,60 @@ public class ToolHelper {
         ActionListener<TransportPPLQueryResponse> listener
     ) {
         return ActionListener.wrap(r -> { listener.onResponse(TransportPPLQueryResponse.fromActionResponse(r)); }, listener::onFailure);
+    }
+
+    /**
+     * Extracts required parameters from the input parameter map based on tool attributes.
+     * If the attributes contain a list of required parameters, only those parameters are extracted.
+     * Otherwise, all parameters are returned.
+     *
+     * @param parameters The input parameters map to extract from
+     * @param attributes The tool attributes containing required parameter information
+     * @return A map containing only the required parameters or all parameters if no required parameters are specified
+     */
+    public static Map<String, String> extractRequiredParameters(Map<String, String> parameters, Map<String, ?> attributes) {
+        Map<String, String> extractedParameters = new HashMap<>();
+        if (parameters == null) {
+            return extractedParameters;
+        }
+        if (attributes != null && attributes.containsKey(ToolConstants.TOOL_REQUIRED_PARAMS)) {
+            List<String> requiredParameters = StringUtils
+                .parseStringArrayToList((String) attributes.get(ToolConstants.TOOL_REQUIRED_PARAMS));
+            if (requiredParameters != null) {
+                for (String requiredParameter : requiredParameters) {
+                    extractedParameters.put(requiredParameter, parameters.get(requiredParameter));
+                }
+            }
+        } else {
+            extractedParameters.putAll(parameters);
+        }
+        return extractedParameters;
+    }
+
+    /**
+     * Extracts all relevant parameters including required parameters and nested parameters from the 'input' field.
+     * First extracts required parameters, then processes the 'input' field if present.
+     * The 'input' field is expected to contain a JSON string that can be parsed into a map of additional parameters.
+     * Variable substitution is performed on the 'input' field using the format ${parameters.key}.
+     *
+     * @param parameters The input parameters map to extract from
+     * @param attributes The tool attributes containing parameter requirements
+     * @return A map containing all extracted parameters including those from the 'input' field
+     */
+    public static Map<String, String> extractInputParameters(Map<String, String> parameters, Map<String, ?> attributes) {
+        Map<String, String> extractedParameters = extractRequiredParameters(parameters, attributes);
+        if (extractedParameters.containsKey("input")) {
+            try {
+                StringSubstitutor stringSubstitutor = new StringSubstitutor(parameters, "${parameters.", "}");
+                String input = stringSubstitutor.replace(parameters.get("input"));
+                extractedParameters.put("input", input);
+                Map<String, String> inputParameters = gson
+                    .fromJson(input, TypeToken.getParameterized(Map.class, String.class, String.class).getType());
+                extractedParameters.putAll(inputParameters);
+            } catch (Exception exception) {
+                log.info("fail extract parameters from key 'input' due to{}", exception.getMessage());
+            }
+        }
+        return extractedParameters;
     }
 }
