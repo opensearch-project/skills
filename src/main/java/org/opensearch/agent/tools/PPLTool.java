c0917c2335b57b6f0066ec3875fba0559db4994f
xinyual <xinyual@amazon.com>
2025-09-16 10:24:10 +0800

    apply spotless
    Signed-off-by: xinyual <xinyual@amazon.com>

--- src/main/java/org/opensearch/agent/tools/PPLTool.java
+++ src/main/java/org/opensearch/agent/tools/PPLTool.java
@@ -239,7 +239,7 @@ public class PPLTool implements WithModelTool {
                     "prompt",
                     prompt,
                     "mappings",
-                        indexInfo.get(MAPPING_KEY),
+                    indexInfo.get(MAPPING_KEY),
                     "os_version",
                     Version.CURRENT.toString(),
                     "current_time",
