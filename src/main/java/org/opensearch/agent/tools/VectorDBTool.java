f7e7e786d204eb7bb4641a07d690b08e6940f6a1
Tyler Ohlsen <ohltyler@amazon.com>
2026-04-06 15:16:56 -0700

    Fix broken method signature in buildSearchRequest
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- src/main/java/org/opensearch/agent/tools/VectorDBTool.java
+++ src/main/java/org/opensearch/agent/tools/VectorDBTool.java
@@ -96,7 +96,8 @@ public class VectorDBTool extends AbstractRetrieverTool implements WithModelTool
     }
 
     // Runtime params for 'embedding_field' and 'index' take priority over values set at registration time.
-    @Override(Map<String, String> parameters) throws IOException {
+    @Override
+    protected synchronized <T> SearchRequest buildSearchRequest(Map<String, String> parameters) throws IOException {
         String originalEmbeddingField = this.embeddingField;
         try {
             if (parameters.containsKey(EMBEDDING_FIELD)) {
